19193dfddfb18a2dc14b3e90be7fb2c27bb77c5b
laura <lauraguntherdesigns@gmail.com>
2022-04-22 12:11:34 -0500

    # rubrics added to readme
    # lamda expression added
    
    # testing in progress

--- src/main/java/capstone/laura/youthmatters/CapstoneApplication.java
+++ src/main/java/capstone/laura/youthmatters/CapstoneApplication.java
@@ -1,5 +1,6 @@
 package capstone.laura.youthmatters;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -12,6 +13,16 @@ public class CapstoneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CapstoneApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> System.out.println("My application got started!!");
     }
 
 }
