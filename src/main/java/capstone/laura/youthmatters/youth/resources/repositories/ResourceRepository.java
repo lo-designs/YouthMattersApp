912d9b4965464aaa471c7f701a338f923dec9891
laura <lauraguntherdesigns@gmail.com>
2022-04-24 13:07:45 -0500

    # added table sorting (by name & program)
    # started custom queries

--- src/main/java/capstone/laura/youthmatters/youth/resources/repositories/ResourceRepository.java
+++ src/main/java/capstone/laura/youthmatters/youth/resources/repositories/ResourceRepository.java
@@ -3,6 +3,7 @@ package capstone.laura.youthmatters.youth.resources.repositories;
 import capstone.laura.youthmatters.youth.resources.models.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -11,9 +12,18 @@ import java.util.List;
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
+
+
     Resource findResourceById(String id);
-//    Set<Resource> findByResourceTagsIn(Collection<Set<ResourceTag>> resourceTags);
-    @Query("select r from Resource r join r.resourceTags t where t.id in ?1")
+    @Query("SELECT r FROM Resource r JOIN r.resourceTags t WHERE t.id in ?1")
     List<Resource> findResourcesByTagIds(Collection<Long> ids);
+
+    Resource getResourceByName(String name);
+    @Query(nativeQuery = true, value = "SELECT * FROM resource r ORDER BY r.name")
+    List<Resource> findResourceByName(Collection<Long> name);
+
+
+
+
 
 }
